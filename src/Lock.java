448004e79ce6171eb429a0828eb91aa8dc08b3e1
Kenneth Castillo <kennethc203@nycstudents.net>
2025-12-09 15:25:40 -0500

    Initial commit

---
+++ src/Lock.java
@@ -0,0 +1,37 @@
+public class Lock {
+    int currentNum = 50;
+    int zeroCount;
+
+    public void rotate(String dir, int num){
+        if (dir.equals("L")){
+            currentNum -= num;
+
+            if (currentNum < 0){
+                currentNum %= 100;
+                if (currentNum < 0) currentNum += 100;
+            }
+        }
+        else if(dir.equals("R")){
+            currentNum += num;
+            if (currentNum > 99)
+                currentNum %= 100;
+        }
+        if (currentNum == 0) zeroCount++;
+    }
+
+    public int getZeroCount(){
+        return zeroCount;
+    }
+    public int getCurrentNum(){
+        return currentNum;
+    }
+    public void setCurrentNum(int i){
+        currentNum = i;
+    }
+
+    public void reset(){
+        currentNum %= 10000;
+        currentNum %= 1000;
+        currentNum %= 100;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("Input.txt");
+        Scanner s = new Scanner(f);
+        Lock santa = new Lock();
+        String total;
+        String dir;
+        int num;
+        int count = 0;
+        while (s.hasNext()) {
+            count++;
+            total = s.nextLine();
+            total = total.substring(0, 1) + " " + total.substring(1);
+            dir = total.substring(0, 1);
+            num = Integer.parseInt(total.substring(2));
+            santa.rotate(dir, num);
+            System.out.println("Current Number: " + santa.getCurrentNum() + " Line Count: " + count);
+        }
+
+        System.out.println(santa.getZeroCount());
+    }
+}
